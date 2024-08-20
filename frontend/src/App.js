f6daeed936c6d5819d7d9c3ae77f32aa19eb634c
SouroN21 <dasunpriyanveen@gmail.com>
2024-08-20 14:21:49 +0530

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,22 @@
 import axios from "axios";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect } from "react";
+import AddProduct from "./pages/addProduct/AddProduct";
 import { useDispatch } from "react-redux";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
+import { useEffect } from "react";
 import { getLoginStatus } from "./services/authService";
 import { SET_LOGIN } from "./redux/features/auth/authSlice";
-
-// Components
+import { ToastContainer } from "react-toastify";
+import Dashboard from "./pages/dashboard/Dashboard";
 import Layout from "./components/layout/Layout";
 import Sidebar from "./components/sidebar/Sidebar";
-import MarketplaceNavbar from "./components/marketplaceNav/MarketplaceNavbar";
-import { CategoryProvider } from "./customHook/CategoryProvider";
-
-// Pages
-import AddProduct from "./pages/addProduct/AddProduct";
-import Dashboard from "./pages/dashboard/Dashboard";
+import "react-toastify/dist/ReactToastify.css";
 import EditProduct from "./pages/editProduct/EditProduct";
 import Categories from "./pages/marketplace/categories/Categories";
-import ProductDetailsfarmer from "./components/product/productDetails/ProductDetails";
-import ProductDetails from "./pages/marketplace/details/ProductDetails";
+import MarketplaceNavbar from "./components/marketplaceNav/MarketplaceNavbar";
+import { CategoryProvider } from "./customHook/CategoryProvider";
+import ProductDetailsfarmer from "../src/components/product/productDetails/ProductDetails";
+import ProductDetails from "./pages/marketplace//details/ProductDetails";
 import MarketHome from "./pages/marketplace/home/marketHomePage";
 import ProductAdminReport from "./components/productAdminReport/productAdminReport";
 import AssignDelivery from "./components/AssignDelivery";
@@ -38,30 +33,31 @@ import Signup from "./components/Singup";
 import Login from "./components/Login";
 import DeliveryMain from "./components/Delivery/DeliveryMain";
 import ExpertMain from "./components/Expert/ExpertMain";
-import AllFeedbacks from "./components/Allfeedbacks";
-import AddFeedback from "./components/Addfeedback";
-import AcceptFeedbacks from "./components/Acceptfeedbacks";
-import AcceptedFeedbacks from "./components/Acceptedfeedbacks";
-import Cart from "./pages/marketplace/details/Cart";
-import Order from "./pages/marketplace/details/Orders";
+import Allfeedbacks from './components/Allfeedbacks'; 
+import Addfeedback from './components/Addfeedback';
+import AcceptFeedbacks from './components/Acceptfeedbacks';
+import AcceptedFeedbacks from './components/Acceptedfeedbacks';
+import Cart from './pages/marketplace/details/Cart';
+import Order from './pages/marketplace/details/Orders';
 import BiddingList from "./pages/bidding/BiddingList/BiddingList";
 import BiddingDetails from "./pages/bidding/BiddingDetails/BiddingDetails";
 import AddBidding from "./pages/bidding/AddBidding";
 import BiddingForm from "./pages/bidding/BiddingForm";
 import UpdateBiddingForm from "./pages/bidding/UpdateBiddingForm";
 import UpdateBuyer from "./components/marketplaceNav/UpdateBuyer";
-import BidCheckout from "./pages/bidding/bidCheckout/BidCheckout";
+import BidCheckout from './pages/bidding/bidCheckout/BidCheckout';
+import AllFeedbacks from "./components/Allfeedbacks";
 import BarChart from "./components/charts/ProductAdminOverviewTable";
 import Successful from "./pages/marketplace/details/successful";
-import Unsuccessful from "./pages/marketplace/details/unuccessful";
+import Unuccessful from "./pages/marketplace/details/unuccessful";
+import OrdersPage from "./pages/orders/OrdersPage";
 
-// Axios default configuration
+
 axios.defaults.withCredentials = true;
 let user;
 
 function App() {
   const dispatch = useDispatch();
-
   useEffect(() => {
     async function loginStatus() {
       const status = await getLoginStatus();
@@ -114,22 +110,17 @@ function App() {
             {user && user.role === "expert" && (
               <Route path="/*" element={<ExpertMain />} />
             )}
-
-            {/* Public Routes */}
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Navigate replace to="/login" />} />
 
-            {/* Other Routes */}
-            <Route path="/updateBuyer" element={<UpdateBuyer />} />
-            <Route
-              path="/market"
-              element={
-                <MarketplaceNavbar showCategories={true}>
-                  <Categories />
+            <Route path="/updateBuyer" element={<UpdateBuyer />}/>
+            <Route path="/market" element={<MarketplaceNavbar showCategories={true}>
+            <Categories />
                 </MarketplaceNavbar>
               }
             />
+
             <Route
               path="/product/:productId"
               element={
@@ -138,6 +129,7 @@ function App() {
                 </MarketplaceNavbar>
               }
             />
+
             <Route
               path="/dashboard"
               element={
@@ -148,6 +140,7 @@ function App() {
                 </Sidebar>
               }
             />
+
             <Route
               path="/add-product"
               element={
@@ -168,6 +161,7 @@ function App() {
                 </Sidebar>
               }
             />
+
             <Route
               path="/product-detail/:id"
               element={
@@ -178,6 +172,8 @@ function App() {
                 </Sidebar>
               }
             />
+
+
             <Route
               path="/edit-product/:id"
               element={
@@ -188,6 +184,7 @@ function App() {
                 </Sidebar>
               }
             />
+
             <Route
               path="/product-admin"
               element={
@@ -198,6 +195,7 @@ function App() {
                 </Sidebar>
               }
             />
+
             <Route
               path="/assign"
               element={
@@ -208,6 +206,7 @@ function App() {
                 </Sidebar>
               }
             />
+
             <Route
               path="/view"
               element={
@@ -218,6 +217,7 @@ function App() {
                 </Sidebar>
               }
             />
+
             <Route
               path="/list-buddy"
               element={
@@ -228,6 +228,7 @@ function App() {
                 </Sidebar>
               }
             />
+
             <Route
               path="/all-buddies"
               element={
@@ -238,6 +239,8 @@ function App() {
                 </Sidebar>
               }
             />
+
+
             <Route
               path="/appointment-list"
               element={
@@ -288,59 +291,47 @@ function App() {
                 </Sidebar>
               }
             />
-            <Route
-              path="/allfeedback"
-              element={
-                <MarketplaceNavbar showCategories={false}>
-                  <AllFeedbacks />
-                </MarketplaceNavbar>
-              }
-            />
-            <Route path="/add" element={<AddFeedback />} />
+        
+        <Route path="/allfeedback" element=
+            {<MarketplaceNavbar showCategories={false}><AllFeedbacks /></MarketplaceNavbar>} />
+            <Route path="/add" element={<Addfeedback />} />
             <Route path="/accept" element={<AcceptFeedbacks />} />
             <Route path="/accepted-feedbacks" element={<AcceptedFeedbacks />} />
-            <Route path="/successful" element={<Successful />} />
-            <Route path="/unuccessful" element={<Unsuccessful />} />
-            <Route
-              path="/cart"
-              element={
-                <MarketplaceNavbar showCategories={false}>
-                  <Cart />
-                </MarketplaceNavbar>
-              }
-            />
-            <Route
-              path="/orders"
-              element={
-                <MarketplaceNavbar showCategories={false}>
-                  <Order />
-                </MarketplaceNavbar>
-              }
-            />
-            <Route
-              path="/bidding"
-              element={
-                <MarketplaceNavbar showCategories={true}>
-                  <BiddingList />
-                </MarketplaceNavbar>
-              }
-            />
-            <Route
-              path="/bidding/bids"
-              element={
-                <MarketplaceNavbar showCategories={true}>
-                  <BidCheckout />
-                </MarketplaceNavbar>
-              }
-            />
-            <Route
-              path="/bidding/:biddingId"
-              element={
-                <MarketplaceNavbar showCategories={false}>
-                  <BiddingDetails />
-                </MarketplaceNavbar>
-              }
-            />
+
+           <Route path="/successful" element={<Successful />} />
+
+           <Route path="/unuccessful" element={<Unuccessful />} />
+
+            <Route path="/cart" element={
+              <MarketplaceNavbar showCategories={false}>
+                <Cart />
+              </MarketplaceNavbar>
+            } />
+
+            <Route path="/orders" element={
+              <MarketplaceNavbar showCategories={false}>
+                <Order />
+              </MarketplaceNavbar>
+            } />
+
+
+            <Route path="/bidding" element={
+              <MarketplaceNavbar showCategories={true}>
+                <BiddingList />
+              </MarketplaceNavbar>
+            } />
+
+            <Route path="/bidding/bids" element={
+              <MarketplaceNavbar showCategories={true}>
+                <BidCheckout />
+              </MarketplaceNavbar>
+            } />
+
+            <Route path="/bidding/:biddingId" element={
+              <MarketplaceNavbar showCategories={false}>
+                <BiddingDetails />
+              </MarketplaceNavbar>
+            } />
             <Route
               path="/addBidding"
               element={
@@ -362,7 +353,7 @@ function App() {
               }
             />
             <Route
-              path="/updateBiddingForm"
+              path="/updateBiddingForm/:id"
               element={
                 <Sidebar>
                   <Layout>
@@ -371,16 +362,9 @@ function App() {
                 </Sidebar>
               }
             />
-            <Route
-              path="/product-admin-overview-table"
-              element={
-                <Sidebar>
-                  <Layout>
-                    <BarChart />
-                  </Layout>
-                </Sidebar>
-              }
-            />
+
+
+
           </Routes>
         </BrowserRouter>
       </CategoryProvider>
